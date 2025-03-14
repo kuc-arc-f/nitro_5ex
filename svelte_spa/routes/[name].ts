d82261ac3773995d3a800bd52d1039493d737e27
中島浩二 <knaka0209@gmail.com>
2025-03-15 07:55:47 +0900

    up, 0315a2

--- svelte_spa/routes/[name].ts
+++ svelte_spa/routes/[name].ts
@@ -2,8 +2,9 @@ export default eventHandler(async (event) => {
   return `<!DOCTYPE html>
 <html lang="ja">
 <head>
-    <meta charset="UTF-8">
-    <title>Nitro Example</title>
+  <meta charset="UTF-8">
+  <title>Nitro Example</title>
+  <link href="/output.css" rel="stylesheet" />
 </head>
 <body>
     <div id="app"></div>
