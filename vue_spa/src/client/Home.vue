252dea9644d886610f49a57433e0835590261abc
中島浩二 <knaka0209@gmail.com>
2025-03-14 22:26:43 +0900

    up, 0314a9

--- vue_spa/src/client/Home.vue
+++ vue_spa/src/client/Home.vue
@@ -1,9 +1,9 @@
 <template>
   <Head />
   <div class="container mx-auto p-4">
-    <h1 class="text-3xl font-bold mb-4">home</h1>
+    <h1 class="text-3xl font-bold mb-4">Home</h1>
     <hr />
-    <a href="/about">[ about ]</a>
+    <a href="/about">[ About ]</a>
   </div>
 </template>
 
