accbd7862f19a4c17a86a548aee17ad1b20c30e0
中島浩二 <knaka0209@gmail.com>
2025-03-15 12:23:07 +0900

    up, 0315a4

--- vue_spa/src/client/Home.vue
+++ vue_spa/src/client/Home.vue
@@ -1,11 +1,7 @@
-<template>
-  <Head />
-  <div class="container mx-auto p-4">
-    <h1 class="text-3xl font-bold mb-4">Home</h1>
-    <hr />
-    <a href="/about">[ About ]</a>
-  </div>
-</template>
-
 <script setup lang="ts">
+import HomeContent from "../components/Home/HomeContent.vue"
 </script>
+
+<template>
+  <HomeContent />
+</template>
