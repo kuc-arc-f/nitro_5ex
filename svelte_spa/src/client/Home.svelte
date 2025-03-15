294ee47c4caf145bf647ca592fcbc820329a311d
中島浩二 <knaka0209@gmail.com>
2025-03-15 12:44:21 +0900

    up, 0315a5

--- svelte_spa/src/client/Home.svelte
+++ svelte_spa/src/client/Home.svelte
@@ -1,12 +1,10 @@
 <script lang="ts">
-import {link} from 'svelte-spa-router'
+import {link} from 'svelte-spa-router';
+import Content from '../components/Home/Content.svelte'
 </script>
 
 <div>
-  <h1 class="text-3xl font-bold">home</h1>
-  <hr />
-  <span><a href="/about" use:link>[ about ]</a>
-  </span>
+  <Content />
 </div>
 
 <style>
